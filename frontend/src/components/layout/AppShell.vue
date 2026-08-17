c853f7fab725cd0bb00bf9c928fda36a23e8d70a
mhkarami97 <mhkarami97@gmail.com>
2026-08-17 15:58:50 +0330

    comment subscription

--- frontend/src/components/layout/AppShell.vue
+++ frontend/src/components/layout/AppShell.vue
@@ -59,10 +59,10 @@
         <span class="text-xs font-bold whitespace-nowrap nav-text hidden">آمار</span>
       </RouterLink>
       
-      <RouterLink v-if="role === 'shop_owner'" class="flex-shrink-0 flex items-center gap-2 rounded-full px-3 py-2.5 text-neutral-400 hover:text-primary transition-all duration-300 active-nav-item" active-class="bg-primary text-white shadow-md shadow-primary/20" to="/subscriptions">
+      <!-- <RouterLink v-if="role === 'shop_owner'" class="flex-shrink-0 flex items-center gap-2 rounded-full px-3 py-2.5 text-neutral-400 hover:text-primary transition-all duration-300 active-nav-item" active-class="bg-primary text-white shadow-md shadow-primary/20" to="/subscriptions">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
         <span class="text-xs font-bold whitespace-nowrap nav-text hidden">اشتراک</span>
-      </RouterLink>
+      </RouterLink> -->
     </nav>
   </div>
 </template>
